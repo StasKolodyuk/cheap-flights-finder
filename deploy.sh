3e43e90b1bb23bb3b21c3c154ff43cfa64f4be91
Kaladziuk, Stanislau <stanislau.kaladziuk@ihg.com>
2018-01-05 19:11:45 +0300

    Restart Unix Service

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 scp target/cheap-flights-finder.jar Stanislau_Kaladziuk@epam.com@ecsc00a0116d.epam.com:/var/apps/cheap-flights-finder
+ssh Stanislau_Kaladziuk@epam.com@ecsc00a0116d.epam.com systemctl restart cheap-flights-finder
