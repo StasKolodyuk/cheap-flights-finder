124895cf79e2487d031c0dda74db49e7b034b9ed
Kaladziuk, Stanislau <stanislau.kaladziuk@ihg.com>
2018-10-01 18:17:11 +0300

    Add Kaunas and Warsaw and increase price to 100 euros

--- src/main/java/by/kolodyuk/cheapflightsfinder/api/FlightCriteria.java
+++ src/main/java/by/kolodyuk/cheapflightsfinder/api/FlightCriteria.java
@@ -8,9 +8,12 @@ public class FlightCriteria {
 
     public static final String MINSK_IATA_CODE = "MSQ";
     public static final String VILNIUS_IATA_CODE = "VNO";
+    public static final String KAUNAS_IATA_CODE = "KUN";
+    public static final String WARSAW_MODLIN_IATA_CODE = "WMI";
+    public static final String WARSAW_CHOPIN_IATA_CODE = "WAW";
 
-    public static final double DEFAULT_MAX_PRICE = 50.0;
-    public static final List<String> DEFAULT_STARTING_POINT_IATA_CODES = asList(MINSK_IATA_CODE, VILNIUS_IATA_CODE);
+    public static final double DEFAULT_MAX_PRICE = 100.0;
+    public static final List<String> DEFAULT_STARTING_POINT_IATA_CODES = asList(MINSK_IATA_CODE, VILNIUS_IATA_CODE, KAUNAS_IATA_CODE, WARSAW_MODLIN_IATA_CODE, WARSAW_CHOPIN_IATA_CODE);
     public static final int DEFAULT_DURATION_FROM_DAYS = 1;
     public static final int DEFAULT_DURATION_TO_DAYS = 3;
 
